04ba59da8f5cd0ea1795def286d0e21c79c9b427
Vassili Korotkine <vassili.korotkine@mail.mcgill.ca>
2024-06-10 16:25:02 -0400

    WIP

--- scripts/bash_scripts_paper_results/point_set_registration_postprocess.sh
+++ scripts/bash_scripts_paper_results/point_set_registration_postprocess.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=${CUR_DIR}/
 PROJECT_ROOT_DIR=${SCRIPT_DIR}../../
 CASE_SCRIPT_DIR=${SCRIPT_DIR}../point_set_registration/
 TOP_RESULT_DIR=${PROJECT_ROOT_DIR}mc_results
-MIXTURE_APPROACHES=("MM" "SM" "MSM" "HSM" "HSM_STD_NO_COMPLEX")
+MIXTURE_APPROACHES=("MM" "SM" "MSM" "HSM_STD_NO_COMPLEX" )
 
 python3 ${CASE_SCRIPT_DIR}3_monte_carlo_postprocess.py --top_result_dir ${TOP_RESULT_DIR}\
     --monte_carlo_from_dim_string "psr_{}d_{}_full"\
--- scripts/bash_scripts_paper_results/vanilla_monte_carlo_postprocess.sh
+++ scripts/bash_scripts_paper_results/vanilla_monte_carlo_postprocess.sh
@@ -9,6 +9,6 @@ TOP_RESULT_DIR=${PROJECT_ROOT_DIR}mc_results
 
 python3 ${CASE_SCRIPT_DIR}2_monte_carlo_postprocess.py \
     --monte_carlo_from_dim_strings vanilla_many_components_{}d_near_step\
-    --mc_run_names Near --dims_list 1 2 --mixture_approaches "MM" "SM" "MSM" "HSM" "HSM_STD_NO_COMPLEX"\
+    --mc_run_names Near --dims_list 1 2 --mixture_approaches "MM" "SM" "MSM" "HSM_STD_NO_COMPLEX"\
     --top_result_dir ${TOP_RESULT_DIR}\
     --read_metrics_csv # if already ran once, csv is saved that can be loaded for metrics
